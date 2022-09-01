8d788c8027bb24c8978b69e74b0b017a96b9d13c
Keith Summers <keith.summers@zeninternet.co.uk>
2022-09-01 14:49:02 +0100

    resolving merge

--- connect.py
+++ connect.py
@@ -1,12 +1,23 @@
 from netmiko import ConnectHandler
 from getpass import getpass
 
-host = {
-    "host" : "cisco3.lasthop.io",
+host_list = [
+    {
+    "host" : "nxos1.lasthop.io",
     "username": "pyclass",
     "password": getpass(),
-    "device_type" : "cisco_xe"
-}
+    "device_type" : "cisco_nxos"
+    },
+    {
+    "host" : "nxos2.lasthop.io",
+    "username": "pyclass",
+    "password": getpass(),
+    "device_type" : "cisco_nxos"
+    }
+]
 
-connect = ConnectHandler(**host)
-print(connect.find_prompt())
+
+for host in host_list:
+    connect = ConnectHandler(**host)
+    print(connect.send_command("show version"))
+    print(connect.find_prompt())
