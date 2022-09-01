7530c4637b52666fc7d0317e3aa9f10195722325
Keith Summers <keith.summers@zeninternet.co.uk>
2022-09-01 14:33:41 +0100

    added initial test

---
+++ connect.py
@@ -0,0 +1,12 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+host = {
+    "host" : "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": getpass(),
+    "device_type" : "cisco_ios"
+}
+
+connect = ConnectHandler(**host)
+print(connect.find_prompt())
