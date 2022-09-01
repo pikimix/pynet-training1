77ecd7af77508caf562f4255d4eaec5426be89b9
Keith Summers <keith.summers@zeninternet.co.uk>
2022-09-01 15:00:30 +0100

    adding second file which saves output instead of printing

---
+++ show_version_save.py
@@ -0,0 +1,18 @@
+from netmiko import ConnectHandler
+from getpass import getpass
+
+host ={
+    "host" : "cisco3.lasthop.io",
+    "username": "pyclass",
+    "password": getpass(),
+    "device_type" : "cisco_iosxe"
+    }
+
+command_output = ""
+
+connect = ConnectHandler(**host)
+
+command_output = connect.send_command("show version")
+
+with open(f"{host['host']}.txt", "w") as file:
+    file.write(command_output)
